bf18251376105d5f44e9c449fd04272fbf8970ff
CandaceAndrews <candacewolfandrews@hotmail.com>
2023-11-28 16:21:20 -0500

    Nesting Routes

--- vue_single_page_app/vue-start-spa/src/App.vue
+++ vue_single_page_app/vue-start-spa/src/App.vue
@@ -3,26 +3,14 @@
 
     <router-view></router-view>
 
-    <!-- <page-viewer
-        v-if="pages.length > 0"
-        :page="pages[activePage]"
-    ></page-viewer>
-
-    <create-page
-    @page-created="pageCreated"
-    ></create-page> -->
 </template>
 
 <script>
 import Navbar from './components/Navbar.vue';
-import PageViewer from './components/PageViewer.vue';
-import CreatePage from './components/CreatePage.vue';
 
 export default {
     components: {
         Navbar,
-        PageViewer,
-        CreatePage
     },
     methods: {
         pageCreated(pageObj) {
