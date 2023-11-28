f62f01458ca1d3a3421cdd5cd782070227b78465
CandaceAndrews <candacewolfandrews@hotmail.com>
2023-11-28 18:40:54 -0500

    Composition API

--- vue_single_page_app/vue-start-spa/src/views/PagesList.vue
+++ vue_single_page_app/vue-start-spa/src/views/PagesList.vue
@@ -1,3 +1,14 @@
 <template>
-    <p>This is the pages list</p>
+    <p>{{data.counter}}</p>
+    <button @click.prevent="click">click</button>
 </template>
+
+<script setup>
+import {ref, reactive} from 'vue';
+const data = reactive({counter:0});
+
+function click() {
+    data.counter++;
+}
+
+</script>
