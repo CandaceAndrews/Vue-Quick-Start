d4c0ca54fc08343aefff7bb49b656a3528a47b8b
CandaceAndrews <candacewolfandrews@hotmail.com>
2023-11-29 16:25:58 -0500

    Injecting Dependencies Into Components

--- vue_single_page_app/vue-start-spa/src/views/PageEdit.vue
+++ vue_single_page_app/vue-start-spa/src/views/PageEdit.vue
@@ -1,3 +1,12 @@
 <template>
-    Edit Page
+    Edit {{route.params.index}}
 </template>
+
+<script setup>
+import { useRoute } from 'vue-router';
+
+const route = useRoute();
+
+const props = defineProps(['index']);
+
+</script>
