e4f347f9aadab651eb57ef40ab551ec53dfc13fb
CandaceAndrews <candacewolfandrews@hotmail.com>
2023-11-29 16:46:32 -0500

    Binding Data and Working with Forms

--- vue_single_page_app/vue-start-spa/src/views/PageEdit.vue
+++ vue_single_page_app/vue-start-spa/src/views/PageEdit.vue
@@ -1,12 +1,85 @@
 <template>
-    Edit {{route.params.index}}
+    <h4>Edit {{page.pageTitle}}</h4>
+
+    <form action="" class="container mb-3">
+        <div class="row">
+            <div class="col-md-8">
+                <div class="mb-3">
+                    <label for="" class="form-label">
+                        Page Title
+                    </label>
+                    <input
+                        type="text"
+                        class="form-control"
+                        v-model="page.pageTitle"
+                    />
+                </div>
+                <div class="mb-3">
+                    <label for="" class="form-label">
+                        Content
+                    </label>
+                    <textarea
+                        type="text"
+                        class="form-control"
+                        rows="5"
+                        v-model="page.content"
+                    ></textarea>
+                </div>
+            </div>
+
+            <div class="col">
+                <div class="mb-3">
+                    <label for="" class="form-label">
+                        Link Text
+                    </label>
+                    <input
+                        type="text"
+                        class="form-control"
+                        v-model="page.link.text"
+                    />
+                </div>
+
+                <div class="row mb-3">
+                    <div class="form-check">
+                        <input 
+                            class="form-check-input" 
+                            type="checkbox" 
+                            v-model="page.published"
+                        >
+                        <label class="form-check-label" for="gridCheck1">
+                        Published
+                        </label>
+                    </div>
+                </div>
+            </div>
+            
+            <div class="mb-3">
+                <button
+                    class="btn btn-secondary me-2"
+                    @click.prevent="submit"
+                >Edit</button>
+                <button
+                    class="btn btn-secondary"
+                    
+                >Cancel</button>
+            </div>
+        </div>
+    </form>
 </template>
 
 <script setup>
 import { useRoute } from 'vue-router';
+import { inject } from 'vue';
 
-const route = useRoute();
+const router = useRoute();
+const pages = inject('$pages');
 
-const props = defineProps(['index']);
+const {index} = defineProps(['index']);
+
+let page = pages.getSinglePage(index);
+
+function submit() {
+    pages.editPage(index, page);
+}
 
 </script>
