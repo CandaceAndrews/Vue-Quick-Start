4f50bc535a4e2ab3e0494557d145ee4843f2efb6
CandaceAndrews <candacewolfandrews@hotmail.com>
2023-11-30 22:04:47 -0500

    Defining Computed and Watched Values

--- vue_single_page_app/vue-start-spa/src/views/PageEdit.vue
+++ vue_single_page_app/vue-start-spa/src/views/PageEdit.vue
@@ -59,9 +59,13 @@
                     @click.prevent="submit"
                 >Edit</button>
                 <button
-                    class="btn btn-secondary"
+                    class="btn btn-secondary me-2"
                     @click.prevent="goToPagesList"
                 >Cancel</button>
+                <button
+                    class="btn btn-danger"
+                    @click.prevent="deletePage"
+                >Delete</button>
             </div>
         </div>
     </form>
@@ -86,6 +90,14 @@ function submit() {
         index,
         page
     });
+
+    goToPagesList();
+}
+
+function deletePage() {
+    pages.removePage(index);
+
+    bus.$emit('page-deleted', {index});
 
     goToPagesList();
 }
